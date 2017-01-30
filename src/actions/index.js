767c9028054c3f478373979eb4d60acb786453f7
Dario Manuel Pfeng Villanueva <dariomanuelpfengvillanueva@MacBook-Pro-de-Dario.local>
2017-01-30 14:19:31 -0300

    Promise Error

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '67a3282fb263b20b2e439f524f8e6e69';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  console.log(request);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
